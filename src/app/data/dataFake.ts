6d052b07505c359b22eccef4aadc0b3649d329ce
Ivan Clay <ivan.clay.moura@gmail.com>
2023-12-07 21:44:22 -0300

    feat: update images

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -16,14 +16,14 @@ export const data =
   {
     "id": "3",
     "photo": "https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/3474/283474-h",
-    "title": "Nova temporada de Bones chegou",
-    "description": "Nova temporada bombástica de Bones ZZZ"
+    "title": "Bones Temporada 6",
+    "description": "Disponível a 6ª temporada de Bones!"
   },
   {
     "id": "4",
-    "photo": "https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/3474/283474-h",
-    "title": "Nova temporada de Bones chegou",
-    "description": "Nova temporada bombástica de Bones ZZZ"
+    "photo": "https://www.ofuxico.com.br/wp-content/uploads/2023/07/bones-serie-tv-disney.jpg",
+    "title": "Bones Temporada 5",
+    "description": "Disponível a 5ª temporada de Bones!"
   },
   {
     "id": "5",
